4b2c9aa409512cdfe72f391d4053c3ddedde9a4f
Rishabh Singh <57044551+gitRishabh-singh@users.noreply.github.com>
2020-05-21 17:41:43 +0530

    uploaded

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function signUp() {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+    })
+}
+
+function logIn() {
+    Swal.fire({
+        icon: 'success',
+        title: 'Done!',
+        text: 'Login Successfully',
+    })
+}
+
